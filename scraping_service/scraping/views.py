435e60e0dd3a9f8f39180031862685a200311200
samsunggora <samsunggora@gmail.com>
2020-05-26 17:17:34 +0200

    add new app

--- scraping_service/scraping/views.py
+++ scraping_service/scraping/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .forms import FindForm
 from .models import Vacancy
 
 
 def home_view(request):
+    form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
     if city or language:
@@ -11,5 +13,9 @@ def home_view(request):
             _filter['city__slug'] = city
         if language:
             _filter['language__slug'] = language
+    else:
+        return render(request, 'scraping/home_views.html')
     qs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/home_views.html', {"object_list": qs}, )
+    return render(request, 'scraping/home_views.html', {"object_list": qs,
+                                                        'form': form,
+                                                        })
--- scraping_service/scraping_service/settings.py
+++ scraping_service/scraping_service/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scraping',
-    
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
