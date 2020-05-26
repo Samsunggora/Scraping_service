b2fda453cafe1ad17da11658ab2cbbe8b02d9d29
samsunggora <samsunggora@gmail.com>
2020-05-26 13:15:39 +0200

    simple display of a set on the site

--- scraping_service/scraping_service/urls.py
+++ scraping_service/scraping_service/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from .views import home
+from scraping.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home, name='home'),
+    path('qs/', home_view, name='home_view'),
 
 ]
